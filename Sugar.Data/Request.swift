7d1f47197f5cce9798b0c88ed2dc71b66e576174
MAXIM KOLESNIK <makleso6@gmail.com>
2018-01-11 03:02:40 +0300

    initial

---
+++ Sugar.Data/Request.swift
@@ -0,0 +1,13 @@
+//
+//  Request.swift
+//  Sugar.Data
+//
+//  Created by MAXIM KOLESNIK on 11.01.2018.
+//  Copyright © 2018 Sugar and Candy. All rights reserved.
+//
+
+import Foundation
+
+struct Request {
+    
+}
